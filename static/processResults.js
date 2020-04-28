066700c3abb0beb71199bd301fa7e54a1b5eb679
Piotr Czajkowski <Piotr.Czajkowski@franklintempleton.com>
2020-04-28 11:26:53 +0200

    Smaller is better

--- static/processResults.js
+++ static/processResults.js
@@ -35,19 +35,18 @@ app.clearFilter = function() {
 	app.filterTable("");
 };
 
+app.getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+
+app.comparer = (idx, asc) => (a, b) => ((v1, v2) => 
+		v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+	)(app.getCellValue(asc ? a : b, idx), app.getCellValue(asc ? b : a, idx));
+
 app.sortTable = function() {
 	// borrowed from https://stackoverflow.com/a/49041392/7342859
-	const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
-
-	const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
-		v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
-	)(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
-
-	// do the work...
 	document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
 		const table = document.getElementById("dataRows");
 		Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
-			.sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+			.sort(app.comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
 			.forEach(tr => table.appendChild(tr) );
 
 		const sorted = " (sorted)";
