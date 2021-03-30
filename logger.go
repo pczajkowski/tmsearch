7545808861c7c86d7fccd588aedef0a9c4c8d794
Piotr Czajkowski <pczajkowski@gmail.com>
2021-03-30 11:55:58 +0200

    Don't hide errors

--- logger.go
+++ logger.go
@@ -16,7 +16,8 @@ func getWriter() (*csv.Writer, *os.File) {
 	logFile := filepath.Join("log", (time.Now().Format(dateFormat) + ".log"))
 	logOutput, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
 	if err != nil {
-		log.Fatalf("Log file error: %s", err)
+		log.Printf("Log file error: %s", err)
+		return nil, nil
 	}
 
 	writer := csv.NewWriter(logOutput)
@@ -25,8 +26,22 @@ func getWriter() (*csv.Writer, *os.File) {
 
 func writeLog(info SearchInfo) {
 	writer, file := getWriter()
+	if writer == nil || file == nil {
+		return
+	}
 
 	writer.Write(info.ToArray())
+	if err := writer.Error(); err != nil {
+		log.Printf("Error writing csv: %s", err)
+	}
+
 	writer.Flush()
+	if err := writer.Error(); err != nil {
+		log.Printf("Error flushing csv: %s", err)
+	}
+
 	file.Close()
+	if err := file.Close(); err != nil {
+		log.Printf("Error closing csv: %s", err)
+	}
 }
