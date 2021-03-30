a840285a6e65b619d33c6490b325c7ce4f29148d
Piotr Czajkowski <pczajkowski@gmail.com>
2021-03-30 12:12:28 +0200

    Some improvements

--- search_test.go
+++ search_test.go
@@ -25,14 +25,13 @@ func TestSegmentCleanup(t *testing.T) {
 	}
 }
 
-func ServeSearchResults() *httptest.Server {
+func serveSearchResults() *httptest.Server {
 	searchResults, err := ioutil.ReadFile("./testFiles/searchResults.json")
 	if err != nil {
-		log.Fatalf("Error reading file: %v\n", err)
+		log.Fatalf("Error reading file: %s\n", err)
 	}
 
 	f := func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(200)
 		w.Header().Set("Content-Type", "application/json")
 		fmt.Fprintln(w, string(searchResults))
 	}
@@ -41,7 +40,7 @@ func ServeSearchResults() *httptest.Server {
 }
 
 func TestSearch(t *testing.T) {
-	server := ServeSearchResults()
+	server := serveSearchResults()
 	defer server.Close()
 
 	var app Application
@@ -49,27 +48,25 @@ func TestSearch(t *testing.T) {
 
 	tmsJSON, err := os.Open("./testFiles/tms.json")
 	if err != nil {
-		t.Fatalf("error reading tms: %v", err)
-		return
+		t.Fatalf("Error reading tms: %s", err)
 	}
 	defer tmsJSON.Close()
 
 	var tms []TM
 	jsonDecoder(tmsJSON, &tms)
 
-	testSourceSegment1 := "<bpt i='1' type='bold'>{}</bpt>Something Test/ Whatever<ept i='1'>{}</ept>"
-	testSourceSegment2 := "<bpt i='1' type='bold'>{}</bpt>Another Test/ Anything<ept i='1'>{}</ept>"
-
-	t.Log("Testing search method.")
 	searchResults := app.search(tms, "something")
 	if searchResults.TotalResults != 4 {
-		t.Fatalf("Not all results returned! (%v)", searchResults.TotalResults)
+		t.Fatalf("Not all results returned! (%d)", searchResults.TotalResults)
 	}
 
-	segment := searchResults.Results[0].Segments[0]
-	if segment.Source == testSourceSegment1 || segment.Source == testSourceSegment2 {
-		t.Log("Search results fine!")
-	} else {
-		t.Fatalf("Something is wrong with returned segment!\n%v", segment)
+	testSourceSegments := []string{"<bpt i='1' type='bold'>{}</bpt>Something Test/ Whatever<ept i='1'>{}</ept>",
+		"<bpt i='1' type='bold'>{}</bpt>Another Test/ Anything<ept i='1'>{}</ept>"}
+
+	for index, segment := range searchResults.Results[0].Segments {
+		if segment.Source != testSourceSegments[index] {
+			t.Fatalf("Something is wrong with returned segment!\nShould be:\n%s\nBut is:\n%s",
+				testSourceSegments[index], segment)
+		}
 	}
 }
